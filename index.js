434a5f7cbd9432bc1b25077285e3481216d47bca
Diego Garcia <admin@dydautomatizacion.com>
2023-08-23 16:45:52 -0500

    CRUD add endpoints

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const express = require('express');;
+const express = require('express');
+const routerApi = require('./routes');
+
+const { logErrors, errorHandler } = require('./middlewares/error.handler')
+
 const app = express();
 const port = 3000;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
@@ -10,35 +16,10 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola soy nueva ruta');
 });
 
-app.get('/products', (req, res) => {
-  res.json([
-    {
-      name: 'Product 1',
-      price: 1000
-    },
-    {
-      name: 'Product 2',
-      price: 2000
-    }
-  ]);
-});
+routerApi(app);
 
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({
-    id,
-    name: 'Product id',
-    prices: 3000
-  });
-});
-
-app.get('/categories/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params;
-  res.json({
-    categoryId,
-    productId
-  })
-});
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log('Mi port ' + port);
---
+++ routes/products.router.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const ProductsService = require('./../services/product.service');
+
+const router = express.Router();
+const service = new ProductsService();
+
+router.get('/', async (req, res) => {
+  const products = await service.find();
+  await res.json(products);
+});
+
+// los endpoints de forma especifica siempre van antes de los dinamicos para que tenga un comportamiento adecuado
+router.get('/filter', (req,res) => {
+  res.send('yo soy un filter');
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const product = await service.findOne(id);
+    res.json(product)
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  const body = req.body;
+  const newProduct = await service.create(body);
+  res.status(201).json(newProduct);
+});
+
+router.patch('/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+    const body = req.body;
+    const product = await service.update(id, body)
+    res.json(product);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message
+    })
+  }
+});
+
+router.delete('/:id', async (req,res)=>{
+  const {id} = req.params;
+  const rta = await service.delete(id);
+  res.json(rta);
+})
+
+module.exports = router;
