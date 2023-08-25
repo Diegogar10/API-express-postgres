306590aa19289a5b4aa41467350208678976eb60
Diego Garcia <admin@dydautomatizacion.com>
2023-08-25 12:43:20 -0500

    API beta v1.0

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler')
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+
+//manejo de acceso a traves de cors
+const whiteList = [
+  'http://localhost:8080',
+  'https://myapp.co',
+];
+const options = {
+  origin: (origin, callback) => {
+    if (whiteList.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido'));
+    }
+  }
+}
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
